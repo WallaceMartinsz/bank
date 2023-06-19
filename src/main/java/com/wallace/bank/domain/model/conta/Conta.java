daaf35655a682ba141750bf5b2a99b27b6f757e2
Wallace Martins <martinsswallace@gmail.com>
2023-06-18 21:57:54 -0300

    chore: add function excluir and add new attribute

--- src/main/java/com/wallace/bank/domain/model/conta/Conta.java
+++ src/main/java/com/wallace/bank/domain/model/conta/Conta.java
@@ -1,5 +1,6 @@
 package com.wallace.bank.domain.model.conta;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.wallace.bank.domain.model.usuario.Usuario;
 import jakarta.persistence.*;
 import lombok.*;
@@ -7,7 +8,6 @@ import lombok.*;
 @Table
 @Entity
 @Getter
-@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode(of = "id")
@@ -22,9 +22,16 @@ public class Conta {
     @Enumerated(EnumType.STRING)
     private TipoConta tipo;
 
+    private boolean ativa;
+
     public Conta(ContaDTO dados) {
+        this.ativa = true;
         this.id = dados.id();
         this.usuario = dados.usuario();
         this.tipo = dados.tipoConta();
+    }
+
+    public void excluir(){
+        this.ativa = false;
     }
 }
