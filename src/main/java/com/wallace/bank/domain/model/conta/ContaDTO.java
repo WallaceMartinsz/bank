89194aa22f49d6b3ad2599e77dbf5b9ee13cfca8
Wallace Martins <martinsswallace@gmail.com>
2023-06-18 21:58:45 -0300

    chore: tweaks in constructon and attribute

--- src/main/java/com/wallace/bank/domain/model/conta/ContaDTO.java
+++ src/main/java/com/wallace/bank/domain/model/conta/ContaDTO.java
@@ -2,8 +2,8 @@ package com.wallace.bank.domain.model.conta;
 
 import com.wallace.bank.domain.model.usuario.Usuario;
 
-public record ContaDTO(Long id, Usuario usuario, TipoConta tipoConta) {
+public record ContaDTO(Long id, Usuario usuario, TipoConta tipoConta, boolean ativa) {
     public ContaDTO(Conta conta){
-        this(conta.getId(), conta.getUsuario(), conta.getTipo());
+        this(conta.getId(), conta.getUsuario(), conta.getTipo(), conta.isAtiva());
     }
 }
