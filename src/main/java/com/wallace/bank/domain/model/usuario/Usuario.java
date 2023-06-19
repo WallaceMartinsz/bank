2ba1ff49a679120f1e212614afbec2a87c8ad913
Wallace Martins <martinsswallace@gmail.com>
2023-06-18 21:59:56 -0300

    chore: add attribute to connect for acount

--- src/main/java/com/wallace/bank/domain/model/usuario/Usuario.java
+++ src/main/java/com/wallace/bank/domain/model/usuario/Usuario.java
@@ -1,12 +1,13 @@
 package com.wallace.bank.domain.model.usuario;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.wallace.bank.domain.model.conta.Conta;
 import jakarta.persistence.*;
 import lombok.*;
 
 @Table
 @Entity
 @Getter
-@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode(of = "id")
@@ -16,5 +17,9 @@ public class Usuario {
     private Long id;
     private String nome;
     private double rendaMensal;
+
+    @OneToOne(mappedBy = "usuario", cascade = CascadeType.ALL)
+    @JsonIgnore
+    private Conta conta;
 
 }
